400e466f84bdc120f787da3a42058759793660f3
Harwin Silva <hsilva@condorlabs.io>
2021-06-16 15:02:10 -0500

    first commit

---
+++ src/filterbyTerm.js
@@ -0,0 +1,9 @@
+function filterByTerm(inputArr, searchTerm) {
+    const regex = new RegExp(searchTerm, "i");
+
+    return inputArr.filter(function (arrayElement) {
+        return arrayElement.url.match(regex);
+    });
+}
+
+module.exports = filterByTerm;
