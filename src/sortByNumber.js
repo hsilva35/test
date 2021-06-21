581216587a56627634ac4343cf1fe6b4a6ef3b4c
Harwin Silva <hsilva@condorlabs.io>
2021-06-21 08:15:07 -0500

    change method

--- src/sortByNumber.js
+++ src/sortByNumber.js
@@ -1,5 +1,5 @@
-function sortByNumber (inputArr) {
-  return inputArr.sort()
+function sortByNumber (inputArr, order) {
+  if (order === 'asc') { return inputArr.sort((a, b) => b - a) } else { return inputArr.sort((a, b) => a - b) }
 }
 
 module.exports = sortByNumber
